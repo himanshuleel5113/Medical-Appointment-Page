69540a0d5b710b68f394a7febee52a23253e34c9
Himanshu Leel <145900790+himanshuleel5113@users.noreply.github.com>
2025-01-29 22:40:42 +0530

    Created using Colab

---
+++ Evenodd.java
@@ -0,0 +1,57 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyMf8o3h+EBJtZf5Nkry1pSI",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/himanshuleel5113/Medical-Appointment-Page/blob/main/Evenodd.java\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "w8wttYgqCoMc"
+      },
+      "outputs": [],
+      "source": [
+        "import java.util.*;\n",
+        "public class Main\n",
+        "{\n",
+        "    public static void main(String[] args)\n",
+        "    {\n",
+        "        Scanner sc = new Scanner(System.in);\n",
+        "        int n = sc.nextInt();\n",
+        "        if(n%2==0)\n",
+        "        {\n",
+        "            System.out.println(\"even\");\n",
+        "        }\n",
+        "        else\n",
+        "        {\n",
+        "            System.out.println(\"odd\");\n",
+        "        }\n",
+        "    }\n",
+        "}"
+      ]
+    }
+  ]
+}
